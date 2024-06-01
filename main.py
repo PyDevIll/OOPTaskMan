e2a940618ca8d9d55e7da8b4f7cc73e55eb22176
PyDevIll <delphadyth@gmail.com>
2024-06-01 14:25:14 +0700

    Finished

--- main.py
+++ main.py
@@ -3,29 +3,36 @@ from typing import List
 
 class Task:
     def __init__(self, title: str, description: str) -> None:
-        self.title = title
-        self.description = description
-
-
-class TaskManager:
-    def __init__(self):
-        tasks = List[]
-        raise NotImplementedError("to be implemented")
-
-    def add_task(self, title: str, description: str) -> None:
-        raise NotImplementedError("to be implemented")
+        self.__title = title
+        self.__description = description
 
     @property
-    def tasks(self) -> List[Task]:
-        raise NotImplementedError("to be implemented")
+    def title(self):
+        return self.__title
 
-if name == '__main__':
+    @property
+    def description(self):
+        return self.__description
+
+
+class TaskManager(object):
+    def __init__(self):
+        self.__tasks = []
+
+    def add_task(self, title: str, description: str) -> None:
+        self.__tasks.append(Task(title, description))
+
+    def get_tasks(self) -> List[Task]:
+        return self.__tasks
+
+
+if __name__ == '__main__':
     task_manager = TaskManager()
 
     task_manager.add_task("Задача 1", "Описание задачи 1")
     task_manager.add_task("Задача 2", "Описание задачи 2")
 
-    tasks = task_manager.tasks
+    tasks = task_manager.get_tasks()
     for idx, task in enumerate(tasks, start=1):
         print(f"Задача {idx}: {task.title} - {task.description}")
 
